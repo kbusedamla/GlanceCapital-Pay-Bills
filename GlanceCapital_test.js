9b2755f66551e26eb33ecc7a09d39dfc7f0aef85
kbusedamla <115742350+kbusedamla@users.noreply.github.com>
2022-10-13 23:37:08 +0300

    Add files via upload

---
+++ GlanceCapital_test.js
@@ -0,0 +1,24 @@
+Feature('Login');
+
+Scenario('test something', ({ I }) => {
+    I.amOnPage('https://dev.glancecapital.com');
+    I.wait(2);
+    I.click('//a[@class="btn btn-link"]');
+    I.wait(2);
+    I.fillField('//input[@id="input-email"]',"osman.saral+70@gmail.com");
+    I.fillField('//input[@id="input-password"]',"123qweASD");
+    I.click('//button[@id="btn-continue"]');
+    I.wait(2);
+    I.click('//button[text()="Pay Bills"]');
+    I.click('//div[@class="react-select__indicators css-1wy0on6"]');
+    I.wait(2)
+    I.click('//div[@id="react-select-2-listbox"]');
+    I.click('//button[text()="Next"]');
+    I.fillField('//input[@class="form-control"]',"1");
+    I.click('//button[text()="Next"]');
+
+
+
+
+
+});
